5b5357643ec352902f48064faebc55a81d298558
24578067 <antyusindaniil@gmail.com>
2023-11-27 16:27:12 +0300

    lab2
    lab2

---
+++ lab2/ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+public class MyDll
+
+{
+    
+
+    
+    public static int Minimum(int a, int b)
+    {
+        return (a < b) ? a : b;
+    }
+
+    
+    public static string NumberToWords(int number)
+    {
+        if (number < 0 || number > 1000)
+        {
+            return "Некорректное число";
+        }
+
+        string[] units = { "", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", "десять",
+                           "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать",
+                           "семнадцать", "восемнадцать", "девятнадцать" };
+        string[] tens = { "", "", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят",
+                          "восемьдесят", "девяносто" };
+        string[] hundreds = { "", "сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот",
+                              "восемьсот", "девятьсот" };
+        string[] thousands = {  "тысяча",  };
+
+        if (number == 0)
+        {
+            return "ноль";
+        }
+
+        string resultString = "";
+
+        if (number >= 100 && number <= 999)
+        {
+            resultString += hundreds[number / 100] + " ";
+            number %= 100;
+        }
+
+        if (number >= 20 && number <= 99)
+        {
+            resultString += tens[number / 10] + " ";
+            number %= 10;
+        }
+
+        if (number >= 1 && number <= 19)
+        {
+            resultString += units[number] + " ";
+        }
+
+        if (number >= 1000)
+        {
+            int thousandsCount = number / 1000;
+            resultString += units[thousandsCount] + " " + thousands[GetThousandForm(thousandsCount)] + " ";
+        }
+
+        return resultString.Trim();
+    }
+
+    private static int GetThousandForm(int number)
+    {
+        number %= 100;
+
+        if ((number >= 11) && (number <= 19))
+        {
+            return 3;
+        }
+
+        int lastDigit = number % 10;
+
+        if (lastDigit == 1)
+        {
+            return 1;
+        }
+        else if (lastDigit >= 2 && lastDigit <= 4)
+        {
+            return 2;
+        }
+        else
+        {
+            return 3;
+        }
+    }
+}
---
+++ lab2/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static void Main()
+        {
+            string author = "Автор: Торгонин Павел";
+            Console.WriteLine(author);
+            Console.WriteLine(MyDll.Minimum(1, 45));
+            Console.WriteLine(MyDll.NumberToWords(777));
+
+            Console.ReadLine();
+        }
+    }
+}
